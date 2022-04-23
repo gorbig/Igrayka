a82890c697ea3b92d054ffcec803e45937f4fde3
IkariPandora <98650407+IkariPandora@users.noreply.github.com>
2022-04-23 12:07:53 +0600

    FirtsPostCheck

--- router/regRouter.js
+++ router/regRouter.js
@@ -1,9 +1,12 @@
 const Router = require('express')
 const router = new Router()
 const controller = require('./regController')
+const {check} = require("express-validator")
 
-
-router.post('/registration', controller.registration)
+router.post('/registration',[
+    check('username',"Name can't be empty").notEmpty(),
+    check('password',"The password must be > 4 and < 10 symbols").isLength({min:4, max:10})
+], controller.registration)
 router.post('/login',controller.login)
 router.get('/users',controller.getUsers)
 
--- server.js
+++ server.js
@@ -1,16 +1,32 @@
 const express = require('express')
-const app = express()
+const mongoose = require('mongoose')
+const regRoutes = require('./router/regRouter')
 const port = 3000
+const app = express()
+app.use(express.json())
+app.use("/reg",regRoutes)
+
+
 
 app.use('/css',express.static(__dirname +'/css'));
-app.use('/image', express.static(__dirname+"/image"))
-app.use('/js', express.static(__dirname+"/js"))
+app.use('/image', express.static(__dirname+"/image"));
+app.use('/js', express.static(__dirname+"/js"));
 
 app.use("/", require("./routes/"));
 app.use("/arrivals", require("./routes/arrivals"));
 app.use("/blogs", require("./routes/blogs"));
 app.use("/index", require("./routes/index"));
 
-app.listen(port, () =>
-    console.log(`App listening at http://localhost:${port}`)
-);
+
+const start = async () => {
+    try{
+        await mongoose.connect('mongodb+srv://qwert:qwert@cluster0.6c0yg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+        app.listen(port, () =>
+            console.log(`App listening at http://localhost:${port}`)
+        );}
+    catch (e) {
+        console.log(e)
+    }
+
+}
+start()
