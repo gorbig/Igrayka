a3c6288d29df73bce91038ecf3fcde79f8acd564
IkariPandora <98650407+IkariPandora@users.noreply.github.com>
2022-04-23 12:07:53 +0600

    FirtsPostCheck

--- router/regController.js
+++ router/regController.js
@@ -1,10 +1,24 @@
-class regController{
-    async registration(req,res){
-      try{
+const User = require('./models/User')
+const Role = require('./models/Role')
 
+class regController{
+    async registration(req,res)
+    {
+      try{
+          const {username,password} = req.body
+          const candidate = await User.findOne({username})
+          if(candidate){
+              return res.status(400).json({message:"a user with the same name already exists"})
+          }
+          const hashPassword = bcrypt.hashSync(password, 7);
+          const userRole = await Role.findOne({value:"USER"})
+          const user = new User({username, password:hashPassword, roles: [userRole.value]})
+          await user.save()
+          return res.json({message:"Пользователь успешно зарегистрировался"})
       }
       catch (e){
-
+        console.log(e)
+          res.status(400).json({message:'Registration error'})
       }
     }
 
@@ -13,12 +27,17 @@ class regController{
 
       }
       catch (e){
-
+          console.log(e)
+          res.status(400).json({message:'Login error'})
       }
     }
 
     async getUsers(req,res){
       try{
+          const userRole = new Role()
+          const adminRole = new Role({value:'ADMIN'})
+          await userRole.save()
+          await adminRole.save()
           res.json("server work")
       }
       catch (e){
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
-const app = express()
+const mongoose = require('mongoose')
+const regRoutes = require('./router/regRouter')
 const port = 3000
+const app = express()
+app.use(express.json())
+app.use("/reg",regRoutes)
 
 app.use('/css',express.static(__dirname +'/css'));
 app.use('/image', express.static(__dirname+"/image"))
@@ -11,6 +15,16 @@ app.use("/arrivals", require("./routes/arrivals"));
 app.use("/blogs", require("./routes/blogs"));
 app.use("/index", require("./routes/index"));
 
-app.listen(port, () =>
-    console.log(`App listening at http://localhost:${port}`)
-);
+
+const start = async () => {
+    try{
+        await mongoose.connect('mongodb+srv://qwert:qwert@cluster0.6c0yg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+        app.listen(port, () =>
+            console.log(`App listening at http://localhost:${port}`)
+        );}
+    catch (e) {
+        console.log(e)
+    }
+
+}
+start()
